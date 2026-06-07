591d5e954c2c53d2e1aea312e050c8668e8e8912
RezaAlam20 <rzaalam384@gmail.com>
2026-06-07 15:29:23 +0330

    fixed the views directory linked path

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const indexRouter = require("./routes/index");
-
+const path = require("path");
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
-const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
