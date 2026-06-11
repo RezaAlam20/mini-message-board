9a3e91688e2cb88410574d2e93a416feb9b85537
RezaAlam20 <rzaalam384@gmail.com>
2026-06-11 23:18:58 +0330

    added validator and conditional checks before posting the request

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,4 +1,5 @@
 const queries = require("../../db/queries");
+const { body, validationResult, matchedData } = require("express-validator");
 const usersGET = async (req, res) => {
   const rows = await queries.getUserMessages();
   res.render("index", { rows });
@@ -8,13 +9,33 @@ const formGet = (req, res) => {
   res.render("form");
 };
 
+const validator = [
+  body("messageUser")
+    .trim()
+    .isLength({ min: 1, max: 20 })
+    .isAlpha()
+    .withMessage("name should be alphabet and between 1 to 20 characters"),
+  body("messageText")
+    .trim()
+    .isLength({ min: 1, max: 255 })
+    .withMessage("text should be at least 1 and max 255 characters long"),
+];
+
 const formPost = async (req, res) => {
-  const { messageUser, messageText } = req.body;
-  await queries.addUser(messageUser, messageText);
-  res.redirect("/");
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    const { messageUser, messageText } = req.body;
+    await queries.addUser(messageUser, messageText);
+    res.redirect("/");
+  } else {
+    res.render("form", {
+      errors: errors.array(),
+    });
+  }
 };
 module.exports = {
   usersGET,
   formGet,
   formPost,
+  validator,
 };
