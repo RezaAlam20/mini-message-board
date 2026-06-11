b724864d9be955f1f4aae6844c38ff559ef52274
RezaAlam20 <rzaalam384@gmail.com>
2026-06-11 23:37:13 +0330

    removed a console log

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,5 @@
 const { Pool } = require("pg");
-require("dotenv").config({ path: "../.env" }); // Make sure dotenv is loaded!
+require("dotenv").config({ path: "../.env" });
 
 const url = process.env.DATABASE_URL || process.env.db_url;
 
